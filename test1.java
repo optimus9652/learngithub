15282b80bd598178d0325b147e4f7e4a583526c3
hemanth939 <hemanthku@bhavithatech.com>
2025-06-29 00:59:00 +0530

    commit message

--- test1.java
+++ test1.java
@@ -2,7 +2,7 @@ class solution3 {
     solution3 solution;
     public solution3() {
         solution = new solution3();
-        for(int i = 0;i < 10;i++) {
+        for(int i = 0;i < 100;i++) {
             
         }
         for(int i =0;i < 10;i++) {
