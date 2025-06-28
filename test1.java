c4a5e57c0037f4831edd71daac6fab95af074b8c
hemanth939 <hemanthku@bhavithatech.com>
2025-06-28 19:59:02 +0530

    commit message

--- test1.java
+++ test1.java
@@ -2,7 +2,7 @@ public class test1 {
     public static void main(String[] args) {
         int count = 0;
         for(int i = 0;i < 10;i++) {
-            System.out.println(10);
+            System.out.println(11);
         }
     }
 }
